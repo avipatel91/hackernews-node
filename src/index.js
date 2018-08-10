5a4d2bf76678d96f5f97c71db99cfeff44dd0d3a
Avi Patel <avipatel91@gmail.com>
2018-08-09 23:00:21 -0700

    Adding prisma

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
+const { Prisma } = require('prisma-binding')
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info)
+    } 
   },
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      }, info)
     }
   }
 }
@@ -29,6 +23,15 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'generated/prisma.graphql',
+      endpoint: 'https://us1.prisma.sh/avi-patel-28f2cc/database/dev',
+      secret: 'mysecret123',
+      debug: true,
+    })
+  })
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
